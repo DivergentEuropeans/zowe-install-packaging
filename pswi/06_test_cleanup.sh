548a1b11d027218d9cd54875f200031f1c1a8840
mm667937 <marketa.muzikova@broadcom.com>
2024-11-20 15:27:30 +0100

    order
    Signed-off-by: mm667937 <marketa.muzikova@broadcom.com>

--- pswi/06_test_cleanup.sh
+++ pswi/06_test_cleanup.sh
@@ -57,28 +57,6 @@ sh scripts/submit_jcl.sh "$(cat JCL)"
 rm JCL
 
 
-# Unmount and delete
-echo "Unmounting and deleting zFS ${WORK_ZFS}."
-
-echo ${JOBST1} >JCL
-echo ${JOBST2} >>JCL
-echo "//UNMNTZFS EXEC PGM=IKJEFT01,REGION=4096K,DYNAMNBR=50" >>JCL
-echo "//SYSTSPRT DD SYSOUT=*" >>JCL
-echo "//SYSTSOUT DD SYSOUT=*" >>JCL
-echo "//SYSTSIN DD * " >>JCL
-echo "UNMOUNT FILESYSTEM('${WORK_ZFS}') +  " >>JCL
-echo "IMMEDIATE" >>JCL
-echo "/*" >>JCL
-echo "//DELTZFST EXEC PGM=IDCAMS" >>JCL
-echo "//SYSPRINT DD SYSOUT=*" >>JCL
-echo "//SYSIN    DD *" >>JCL
-echo " DELETE ${WORK_ZFS}" >>JCL
-echo "/*" >>JCL
-
-sh scripts/submit_jcl.sh "$(cat JCL)"
-# Not checking results so the script doesn't fail
-rm JCL
-
 
 # Unmount and delete
 echo "Unmounting and deleting zFS ${TEST_HLQ}.ZFS."
@@ -126,3 +104,24 @@ sh scripts/submit_jcl.sh "$(cat JCL)"
 rm JCL
 
 
+# Unmount and delete
+echo "Unmounting and deleting zFS ${WORK_ZFS}."
+
+echo ${JOBST1} >JCL
+echo ${JOBST2} >>JCL
+echo "//UNMNTZFS EXEC PGM=IKJEFT01,REGION=4096K,DYNAMNBR=50" >>JCL
+echo "//SYSTSPRT DD SYSOUT=*" >>JCL
+echo "//SYSTSOUT DD SYSOUT=*" >>JCL
+echo "//SYSTSIN DD * " >>JCL
+echo "UNMOUNT FILESYSTEM('${WORK_ZFS}') +  " >>JCL
+echo "IMMEDIATE" >>JCL
+echo "/*" >>JCL
+echo "//DELTZFST EXEC PGM=IDCAMS" >>JCL
+echo "//SYSPRINT DD SYSOUT=*" >>JCL
+echo "//SYSIN    DD *" >>JCL
+echo " DELETE ${WORK_ZFS}" >>JCL
+echo "/*" >>JCL
+
+sh scripts/submit_jcl.sh "$(cat JCL)"
+# Not checking results so the script doesn't fail
+rm JCL
